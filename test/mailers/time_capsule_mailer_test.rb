f59895340525422038eebf21a858e68a3c5002d4
Juan Mena <juanmamena2005@gmail.com>
2026-08-07 21:26:54 -0500

    fix: add PR size limit and fixed rubocop

--- test/mailers/time_capsule_mailer_test.rb
+++ test/mailers/time_capsule_mailer_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TimeCapsuleMailerTest < ActionMailer::TestCase
   test "magic_link" do
     mail = TimeCapsuleMailer.magic_link("user@example.com", "token123")
-    assert_equal ["user@example.com"], mail.to
+    assert_equal [ "user@example.com" ], mail.to
     assert_equal I18n.t("mailers.magic_link.subject"), mail.subject
   end
 
@@ -17,12 +17,12 @@ class TimeCapsuleMailerTest < ActionMailer::TestCase
     )
     letter.save!(validate: false)
     mail = TimeCapsuleMailer.future_letter(letter)
-    assert_equal ["user@example.com"], mail.to
+    assert_equal [ "user@example.com" ], mail.to
   end
 
   test "confirm_email_update" do
     mail = TimeCapsuleMailer.confirm_email_update("old@example.com", "new@example.com", "token123")
-    assert_equal ["new@example.com"], mail.to
+    assert_equal [ "new@example.com" ], mail.to
     assert_equal I18n.t("mailers.confirm_email_update.subject"), mail.subject
   end
 end
