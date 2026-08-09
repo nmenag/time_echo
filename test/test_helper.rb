46fb95b746c85b3f99bfdb496572e2e753bf548c
Juan Mena <juanmamena2005@gmail.com>
2026-08-08 19:48:29 -0500

    fix: change test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,13 @@
 ENV["RAILS_ENV"] ||= "test"
-require "simplecov"
-SimpleCov.start
 require_relative "../config/environment"
 require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    workers = if ENV["CI"] || ENV["WSL_DISTRO_NAME"] || Gem.win_platform?
-      1
-    else
-      :number_of_processors
-    end
-    parallelize(workers: workers)
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
