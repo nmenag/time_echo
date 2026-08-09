fa40a69fffd083d2ce3886d1bdc4777f863be1e2
Juan Mena <juanmamena2005@gmail.com>
2026-08-08 20:26:39 -0500

    fix: fix simplecov coverage ci

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.active_record.encryption.primary_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY") { Rails.application.credentials.active_record_encryption[:primary_key] }
-  config.active_record.encryption.deterministic_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY") { Rails.application.credentials.active_record_encryption[:deterministic_key] }
-  config.active_record.encryption.key_derivation_salt = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT") { Rails.application.credentials.active_record_encryption[:key_derivation_salt] }
+  config.active_record.encryption.primary_key = Rails.application.credentials[:primary_key]
+  config.active_record.encryption.deterministic_key = Rails.application.credentials[:deterministic_key]
+  config.active_record.encryption.key_derivation_salt = Rails.application.credentials[:key_derivation_salt]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,22 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
 
-    # Add more helper methods to be used by all tests here...
+    parallelize_teardown do |_worker|
+      SimpleCov.result
+    end
+
+    fixtures :all
   end
 end
