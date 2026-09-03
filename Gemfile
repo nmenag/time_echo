c4b5097a1ea7aa5c93683c8546493a58206f83de
Juan Mena <juanmamena2005@gmail.com>
2026-09-03 00:29:11 -0500

    fix(assets): register builds path in propshaft and enable public file server

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem "propshaft"
 # Use pg as the database for Active Record
 gem "pg"
 gem "resend"
-gem "tailwindcss-rails"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
-  # Cache assets for far-future expiry since they are all digest stamped.
+  # Enable serving of static files by the Puma web server in production
+  config.public_file_server.enabled = true
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
