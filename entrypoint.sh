a0313fa6656eb2e0d86c6955e4fce111ad4107d8
nmenag <nmena.garzon@gmail.com>
2026-09-17 23:00:58 -0500

    fix: fix up entrypoint on deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,6 @@ rm -f /rails/tmp/pids/server.pid
 
 bundle check || bundle install
 
-until pg_isready -h db -U postgres; do
-  echo "Waiting for PostgreSQL to start..."
-  sleep 1
-done
-
 bundle exec rails db:prepare
 
 exec "$@"
